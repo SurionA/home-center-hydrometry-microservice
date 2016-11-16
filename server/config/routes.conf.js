e8a716d47018140fdc6a626e770fe2ffadc6c3d3
SurionA <frere.maxime@gmail.com>
2016-11-16 17:26:24 +0100

    #REMOVE# rooms

--- server/config/routes.conf.js
+++ server/config/routes.conf.js
@@ -1,12 +1,10 @@
 import HydrometryRoutes from '../api/hydrometry';
-import RoomsRoutes from '../api/room';
 
 export function initRoutes (app) {
   const startTime = new Date();
 
   // Insert routes below
   app.use('/api/hydrometries', HydrometryRoutes);
-  app.use('/api/rooms', RoomsRoutes);
 
   app.route('/*')
     .get((req, res) => {
